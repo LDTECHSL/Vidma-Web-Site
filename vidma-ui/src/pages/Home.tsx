8af4375d19cd62aac50a53f57504268bdde5270c
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-10-15 16:30:57 +0530

    feat: Enhance hero section with title and updated styles

--- vidma-ui/src/pages/Home.tsx
+++ vidma-ui/src/pages/Home.tsx
@@ -4,7 +4,11 @@ export default function Home() {
     return (
         <Main>
             <div className="hero">
-                <div className="hero-content"></div>
+                <div className="hero-content">
+                    <div className="hero-title">
+                        Ultimate Roofing Services ...
+                    </div>
+                </div>
                 <div className="hero-content hc"></div>
             </div>
         </Main>
