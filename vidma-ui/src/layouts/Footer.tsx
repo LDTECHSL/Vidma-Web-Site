71da9694d8062b89172a5238e4383b3ffb820c86
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-10-24 11:43:56 +0530

    Refactor code structure for improved readability and maintainability

--- vidma-ui/src/layouts/Footer.tsx
+++ vidma-ui/src/layouts/Footer.tsx
@@ -1,4 +1,6 @@
 import "../common/main.css"
+import banner from "../assets/awards-banner.png";
+import banner2 from "../assets/brands-banner.png";
 
 export default function Footer() {
     return (
@@ -6,18 +8,20 @@ export default function Footer() {
             <div className="footer-inner">
                 <div className="footer-section">
                     <div className="f-title">
-                        Get Help
-                    </div>
-                    <div className="f-link">Contact Us</div>
-                    <div className="f-link">WhatsApp</div>
-                </div>
-                <div className="footer-section">
-                    <div className="f-title">
                         Our Services
                     </div>
                     <div className="f-link">Projects</div>
                     <div className="f-link">Products</div>
                     <div className="f-link">About Us </div>
+                    <div className="f-link">Contact Us </div>
+                </div>
+                <div className="footer-section">
+                    <div className="f-title">
+                        Get Help
+                    </div>
+                    <div className="f-link">Contact Us</div>
+                    <div className="f-link">WhatsApp</div>
+                    <div className="f-link">info@vidma.com</div>
                 </div>
                 <div className="footer-section">
                     <div className="f-title">
@@ -31,13 +35,32 @@ export default function Footer() {
                         Follow Us
                     </div>
                     <div className="f-link">Facebook</div>
-                    <div className="f-link">Instagram</div>
-                    <div className="f-link">LinkedIn</div>
+                    <div className="f-link">WhatsApp</div>
+                    <div className="f-link">TikTok</div>
                 </div>
             </div>
 
+            <div className="footer-awards-outer">
+                <div className="f">
+                    <div className="f-title">
+                        Awards & Recognitions
+                    </div>
+
+                    <img className="footer-awards-banner" src={banner} alt="Awards & Recognitions" />
+                </div>
+
+                <div className="f">
+                    <div className="f-title">
+                        Companies
+                    </div>
+
+                    <img className="footer-awards-banner" src={banner2} alt="Awards & Recognitions" />
+                </div>
+
+            </div>
+
             <div className="footer-bottom">
-                
+
             </div>
         </div>
     );
