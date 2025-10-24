15c455747dc5b818e3c2eb9205f55ce26bfaab8f
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-10-24 10:54:46 +0530

    style: Update Footer component layout and add links for improved navigation

--- vidma-ui/src/layouts/Footer.tsx
+++ vidma-ui/src/layouts/Footer.tsx
@@ -4,13 +4,39 @@ export default function Footer() {
     return (
         <div className="footer-outer">
             <div className="footer-inner">
-                <div className="footer-section"></div>
-                <div className="footer-section"></div>
-                <div className="footer-section"></div>
-                <div className="footer-section"></div>
+                <div className="footer-section">
+                    <div className="f-title">
+                        Get Help
+                    </div>
+                    <div className="f-link">Contact Us</div>
+                    <div className="f-link">WhatsApp</div>
+                </div>
+                <div className="footer-section">
+                    <div className="f-title">
+                        Our Services
+                    </div>
+                    <div className="f-link">Projects</div>
+                    <div className="f-link">Products</div>
+                    <div className="f-link">About Us </div>
+                </div>
+                <div className="footer-section">
+                    <div className="f-title">
+                        Legal
+                    </div>
+                    <div className="f-link">Privacy Policy</div>
+                    <div className="f-link">Terms of Service</div>
+                </div>
+                <div className="footer-section">
+                    <div className="f-title">
+                        Follow Us
+                    </div>
+                    <div className="f-link">Facebook</div>
+                    <div className="f-link">Instagram</div>
+                    <div className="f-link">LinkedIn</div>
+                </div>
             </div>
 
-            <div className="footer-bootom">
+            <div className="footer-bottom">
                 
             </div>
         </div>
