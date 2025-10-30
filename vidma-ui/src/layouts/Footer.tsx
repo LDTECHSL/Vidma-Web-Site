2f464cb56278c30cceff5b700a16ce988851fe88
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-10-30 16:12:25 +0530

    feat: enhance Footer component with social media links and improve accessibility

--- vidma-ui/src/layouts/Footer.tsx
+++ vidma-ui/src/layouts/Footer.tsx
@@ -5,26 +5,41 @@ import Divider from "@mui/material/Divider";
 import Lottie from "lottie-react";  // ✅ Import Lottie
 import world from "../assets/world.json"; // ✅ Your animation JSON
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { getContactUsData } from "../services/home-api";
 
 export default function Footer() {
-
+    const [data, setData] = useState<any>({});
     const { t } = useTranslation();
+
+    const getSocialMediaLinks = async () => {
+        try { 
+            const res = await getContactUsData();
+            setData(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getSocialMediaLinks();
+    }, []);
 
     return (
         <div className="footer-outer">
             <div className="footer-inner">
                 <div className="footer-section" data-aos="fade-up">
                     <div className="f-title">{t("services")}</div>
-                    <div className="f-link">{t("gallery")}</div>
-                    <div className="f-link">{t("products")}</div>
-                    <div className="f-link">{t("about")}</div>
-                    <div className="f-link">{t("contact")}</div>
+                    <div className="f-link"><a href="#gallery">{t("gallery")}</a></div>
+                    <div className="f-link"><a href="#products">{t("products")}</a></div>
+                    <div className="f-link"><a href="#about">{t("about")}</a></div>
+                    <div className="f-link"><a href="#contact">{t("contact")}</a></div>
                 </div>
 
                 <div className="footer-section" data-aos="fade-up">
                     <div className="f-title">{t("getHelp")}</div>
-                    <div className="f-link">{t("contact")}</div>
-                    <div className="f-link">{t("whatsApp")}</div>
+                    <div className="f-link"><a href="#contact">{t("contact")}</a></div>
+                    <div className="f-link" onClick={() => window.open(data.whatsappLink, "_blank")}>{t("whatsApp")}</div>
                     {/* <div className="f-link">info@vidma.com</div> */}
                 </div>
 
@@ -36,9 +51,9 @@ export default function Footer() {
 
                 <div className="footer-section" data-aos="fade-up">
                     <div className="f-title">{t("followUs")}</div>
-                    <div className="f-link">{t("facebook")}</div>
-                    <div className="f-link">{t("whatsApp")}</div>
-                    <div className="f-link">{t("tiktok")}</div>
+                    <div className="f-link" onClick={() => window.open(data.facebookLink, "_blank")}>{t("facebook")}</div>
+                    <div className="f-link" onClick={() => window.open(data.whatsappLink, "_blank")}>{t("whatsApp")}</div>
+                    <div className="f-link" onClick={() => window.open(data.tikTokLink, "_blank")}>{t("tiktok")}</div>
                 </div>
             </div>
 
@@ -50,7 +65,7 @@ export default function Footer() {
 
                 {/* ✅ Lottie Animation in center */}
                 <div className="footer-lottie" data-aos="fade-up">
-                    <Lottie 
+                    <Lottie
                         animationData={world}
                         loop={true}
                         autoplay={true}
@@ -68,8 +83,8 @@ export default function Footer() {
 
             <div className="footer-bottom">
                 <div className="fb-text">
-                    © {new Date().getFullYear()} Vidma. All Rights Reserved. | Design and Developed by 
-                    <span style={{ color: "rgba(7, 115, 223, 1)", fontWeight: "bold" }}> 2D-Coders</span>
+                    © {new Date().getFullYear()} Vidma. All Rights Reserved. | Design and Developed by
+                    <span style={{ color: "rgba(7, 115, 223, 1)", fontWeight: "bold", cursor: "pointer" }}> 2D-Coders</span>
                 </div>
             </div>
         </div>
