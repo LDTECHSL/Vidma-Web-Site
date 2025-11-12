854e32c1e26ef748fbc8da2fd84cc19f4bad3b88
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-11-12 15:29:15 +0530

    feat: enhance SEO with meta tags and add Helmet for dynamic head management

--- vidma-ui/src/main.tsx
+++ vidma-ui/src/main.tsx
@@ -6,14 +6,17 @@ import ErrorBoundary from './components/ErrorBoundary.tsx'
 import './i18n';
 import { LanguageProvider } from './components/LanguageContext.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <LanguageProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <HelmetProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </HelmetProvider>
       </LanguageProvider>
     </ErrorBoundary>
   </StrictMode>
