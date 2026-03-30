4cd87bc027b1a22b04743f96e0a1a4aa300d7a31
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-03-30 20:53:53 +0530

    feat: remove Length column from Product table in database migration

---
+++ vidma-backend/VidmaEngineering/Infrastructure/Migrations/20260330152245_remove length column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removelengthcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Product");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Length",
+                table: "Product",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
